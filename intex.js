6d6346a2956209a904c398357b24cbc4a573c334
Abijith P <abhijithunni00@gmail.com>
2024-01-12 19:04:37 +0530

    check addo

--- intex.js
+++ intex.js
@@ -1,6 +1,6 @@
 function handleclick() {
    if( document.getElementById('head').innerHTML == "button clicked"){
-    document.getElementById('head').innerHTML = "button not clicked"
+    document.getElementById('head').innerHTML = "button not yet clicked"
    }else{
     document.getElementById('head').innerHTML = "button clicked"
    }
