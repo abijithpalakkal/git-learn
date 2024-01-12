0e86ebd42e27e43b8664010a136075d420188dce
Abijith P <abhijithunni00@gmail.com>
2024-01-12 19:02:00 +0530

    check

--- intex.js
+++ intex.js
@@ -1,5 +1,5 @@
 function handleclick() {
-   if( document.getElementById('head').innerHTML == "button clicked"){
+   if( document.getElementById('head').innerHTML == "butto clicked"){
     document.getElementById('head').innerHTML = "button not clicked"
    }else{
     document.getElementById('head').innerHTML = "button clicked"
