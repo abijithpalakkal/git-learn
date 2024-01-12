10c212ca473ee3bf5697b7c1f994d2ddc0db01d8
Abijith P <abhijithunni00@gmail.com>
2024-01-12 18:07:50 +0530

    initial commit

---
+++ intex.js
@@ -0,0 +1,7 @@
+function handleclick() {
+   if( document.getElementById('head').innerHTML == "button clicked"){
+    document.getElementById('head').innerHTML = "button not clicked"
+   }else{
+    document.getElementById('head').innerHTML = "button clicked"
+   }
+}
